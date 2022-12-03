b2c43df5f96f396c8e953c2cd781266cc2c33e63
zhenyvwu <zhenyvwu@gmail.com>
2022-12-03 22:12:46 +0800

    add a restful demo

--- todata/__init__.py
+++ todata/__init__.py
@@ -1,6 +1,21 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify, request, abort, current_app, make_response
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
 
 
 def create_app(test_config=None):
@@ -38,5 +53,34 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello World!'
+
+    @app.route('/todata/api/v1.0/tasks', methods=['GET'])
+    def get_tasks():
+        return jsonify({'tasks': tasks})
+
+    @app.route('/todata/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+    def get_task(task_id):
+        task = list(filter(lambda t: t['id'] == task_id, tasks))
+        current_app.logger.info(task)
+        if len(task) == 0:
+            abort(404)
+        return jsonify({'task': task[0]})
+
+    @app.route('/todata/api/v1.0/tasks', methods=['POST'])
+    def create_task():
+        if not request.json or not 'title' in request.json:
+            abort(400)
+        task = {
+            'id': tasks[-1]['id'] + 1,
+            'title': request.json['title'],
+            'description': request.json.get('description', ""),
+            'done': False
+        }
+        tasks.append(task)
+        return jsonify({'task': task}), 201
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return make_response(jsonify({'error': 'Not found'}), 404)
 
     return app
